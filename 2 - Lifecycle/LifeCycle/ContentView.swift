bbdfd40fb78a2d711a523857ffb9788c7b53d254
Ben Chatelain <ben@octop.ad>
2020-03-29 17:57:52 -0600

    ✨ Add LifeCycle project for chapter 2

---
+++ 2 - Lifecycle/LifeCycle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LifeCycle
+//
+//  Created by Ben Chatelain on 3/29/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
