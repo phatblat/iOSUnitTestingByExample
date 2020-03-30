13d8dacc14ea9d145f9f232aea04a03278db9a21
Ben Chatelain <ben@octop.ad>
2020-03-29 20:12:04 -0600

    ✅ Add test_zero

---
+++ 2 - Lifecycle/LifeCycle/MyClass.swift
@@ -0,0 +1,17 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by Ben Chatelain on 3/29/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+class MyClass {
+    func methodOne() {
+        print(">> methodOne")
+    }
+
+    func methodTwo() {
+        print(">> methodTwo")
+    }
+}
---
+++ 2 - Lifecycle/LifeCycleTests/MyClassTests.swift
@@ -0,0 +1,15 @@
+//
+//  MyClassTests.swift
+//  LifeCycleTests
+//
+//  Created by Ben Chatelain on 3/29/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import XCTest
+
+class MyClassTests: XCTestCase {
+    func test_zero() {
+        XCTFail("Tests not yet implemented in MyClassTests")
+    }
+}
