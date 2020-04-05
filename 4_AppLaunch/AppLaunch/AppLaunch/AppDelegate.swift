836497817eba5b3c6c3d85d5ef15ad723e98c4e8
Ben Chatelain <ben@octop.ad>
2020-04-05 15:50:24 -0600

    ♻️ Switch to storyboard and vc from SwiftUI

--- 4_AppLaunch/AppLaunch/AppLaunch/AppDelegate.swift
+++ 4_AppLaunch/AppLaunch/AppLaunch/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(">> Launching with real app delegate")
         return true
---
+++ 4_AppLaunch/AppLaunch/AppLaunch/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AppLaunch
+//
+//  Created by Ben Chatelain on 4/5/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ViewController.viewDidLoad()")
+    }
+}
