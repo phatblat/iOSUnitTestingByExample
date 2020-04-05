903991cc1ef7700ff8cf42c12eea1944be340ab6
Ben Chatelain <ben@octop.ad>
2020-04-05 15:54:12 -0600

    🔊 Log window/root vc on test start

--- 4_AppLaunch/AppLaunch/AppLaunchTests/TestingAppDelegate.swift
+++ 4_AppLaunch/AppLaunch/AppLaunchTests/TestingAppDelegate.swift
@@ -10,9 +10,12 @@ import UIKit
 
 @objc(TestingAppDelegate)
 class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("<< Launching with testing app delegate")
+        print("Window: \(String(describing: window))")
+        print("Root VC: \(String(describing: window?.rootViewController))")
         return true
     }
 }
