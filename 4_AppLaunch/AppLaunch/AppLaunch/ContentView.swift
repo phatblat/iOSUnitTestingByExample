d3b974070f0e9d573ea2612786b7172c9ee52a3b
Ben Chatelain <ben@octop.ad>
2020-04-05 14:45:08 -0600

    🛠 Add AppLaunch project for chapter 4

---
+++ 4_AppLaunch/AppLaunch/AppLaunch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppLaunch
+//
+//  Created by Ben Chatelain on 4/5/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
