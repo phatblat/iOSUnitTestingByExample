6abc36d39777c6de37926577ed9fcc58e82f6b8f
Ben Chatelain <ben@octop.ad>
2020-04-05 19:23:18 -0600

    🛠 Add LoadViewControllers project for chapter 5

---
+++ 5_LoadViewControllers/LoadViewControllers/LoadViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadViewControllers
+//
+//  Created by Ben Chatelain on 4/5/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
