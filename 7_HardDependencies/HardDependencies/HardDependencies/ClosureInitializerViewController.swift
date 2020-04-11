e2822ca0042162e38bba28276eee2353b81e00da
Ben Chatelain <ben@octop.ad>
2020-04-11 09:50:16 -0600

    ✨ Add viewcontrollers

---
+++ 7_HardDependencies/HardDependencies/HardDependencies/ClosureInitializerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClosureInitializerViewController.swift
+//  HardDependencies
+//
+//  Created by Ben Chatelain on 4/11/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ClosureInitializerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
