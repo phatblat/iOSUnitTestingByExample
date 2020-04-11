527e6f7356dd5be177a3788fd59ea9a9807c05de
Ben Chatelain <ben@octop.ad>
2020-04-11 08:52:41 -0600

    🛠 Add HardDependencies project for chapter 7

---
+++ 7_HardDependencies/HardDependencies/HardDependencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HardDependencies
+//
+//  Created by Ben Chatelain on 4/11/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
