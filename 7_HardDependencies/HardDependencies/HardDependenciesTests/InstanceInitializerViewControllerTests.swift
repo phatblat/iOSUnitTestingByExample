089607bfc1570eb2d2974bad92a4e9cb71de626d
Ben Chatelain <ben@octop.ad>
2020-04-11 13:33:41 -0600

    ✅ Add InstanceInitializerViewControllerTests

---
+++ 7_HardDependencies/HardDependencies/HardDependenciesTests/InstanceInitializerViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  InstanceInitializerViewControllerTests.swift
+//  HardDependenciesTests
+//
+//  Created by Ben Chatelain on 4/11/20.
+//  Copyright © 2020 Ben Chatelain. All rights reserved.
+//
+
+@testable import HardDependencies
+import XCTest
+
+class InstanceInitializerViewControllerTests: XCTestCase {
+
+    func test_viewDidAppear() {
+        let sut = InstanceInitializerViewController(analytics: Analytics())
+        sut.loadViewIfNeeded()
+
+        sut.viewDidAppear(false)
+    }
+}
